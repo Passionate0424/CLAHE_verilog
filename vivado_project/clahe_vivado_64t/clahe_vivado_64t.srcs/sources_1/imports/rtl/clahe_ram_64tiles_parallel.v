cee971c0aaf996e7b6d54ed75fa44dc9fcc393e4
Passionate0424 <144249616+Passionate0424@users.noreply.github.com>
2025-12-11 20:41:31 +0800

    update rtl

--- vivado_project/clahe_vivado_64t/clahe_vivado_64t.srcs/sources_1/imports/rtl/clahe_ram_64tiles_parallel.v
+++ vivado_project/clahe_vivado_64t/clahe_vivado_64t.srcs/sources_1/imports/rtl/clahe_ram_64tiles_parallel.v
@@ -1,4 +1,22 @@
 // ============================================================================
+// Copyright (c) 2025 Passionate0424
+// 
+// GitHub: https://github.com/Passionate0424/CLAHE_verilog
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ============================================================================
+
+// ============================================================================
 // CLAHE 64块并行读取RAM架构 - 支持四块并行读取
 //
 // 功能描述:
